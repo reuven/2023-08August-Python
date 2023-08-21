25cc6c7f5eac758a38e86d6144bc266ec4cd9713
Reuven Lerner <reuven@lerner.co.il>
2023-08-21 21:10:46 +0300

    gitautosync update 178

--- ipython_log.py
+++ ipython_log.py
@@ -492,3 +492,8 @@ num2 = input('Enter second number: ')
 total = int(num1) + int(num2)
 
 print(f'{num1} + {num2} = {total}')
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+total = int(num1) + int(num2)
+
+print(f'{num1} + {num2} = {total}')

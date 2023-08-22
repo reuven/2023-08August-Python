b1cc014f640b40b7ce7a5422eaaf35d106770893
Reuven Lerner <reuven@lerner.co.il>
2023-08-22 19:30:53 +0300

    gitautosync update 161

--- ipython_log.py
+++ ipython_log.py
@@ -535,3 +535,14 @@ for word in sentence.split():
         print(word + 'way')
     else:
         print(word[1:] + word[0] + 'ay')
+sentence = input('Enter a sentence: ').strip()
+
+output = []    # empty list for output
+
+for word in sentence.split():
+    if word[0] in 'aeiou':
+        output.append(word + 'way')
+    else:
+        output.append(word[1:] + word[0] + 'ay')
+
+print(output)

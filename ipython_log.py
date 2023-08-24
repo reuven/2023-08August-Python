4e7de157e03d4094d2d389a7a6da54ce34554afb
Reuven Lerner <reuven@lerner.co.il>
2023-08-24 16:33:04 +0300

    gitautosync update 109

--- ipython_log.py
+++ ipython_log.py
@@ -4,3 +4,20 @@
 def myfunc():
     print('Hello!')
 myfunc()
+def calc():
+    n1 = input('Enter first number: ').strip()
+    op = input('Enter operator: ').strip()
+    n2 = input('Enter second number: ').strip()
+
+    n1 = int(n1)
+    n2 = int(n2)
+
+    if op == '+':
+        result = n1 + n2
+    elif op == '-':
+        result = n1 - n2
+    else:
+        result = f'Illegal operator {op}'
+
+    print(f'{n1} {op} {n2} = {result}')
+calc()

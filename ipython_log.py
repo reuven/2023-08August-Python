b5b9835502926eee7768c906791c894f62a26fef
Reuven Lerner <reuven@lerner.co.il>
2023-08-24 16:50:35 +0300

    gitautosync update 122

--- ipython_log.py
+++ ipython_log.py
@@ -50,3 +50,20 @@ print('Hello out there')
 hello()
 # get the value back from hello, and then print it
 print(hello())
+def calc():
+    n1 = input('Enter first number: ').strip()
+    op = input('Enter operator: ').strip()
+    n2 = input('Enter second number: ').strip()
+
+    n1 = int(n1)
+    n2 = int(n2)
+
+    if op == '+':
+        result = n1 + n2
+    elif op == '-':
+        result = n1 - n2
+    else:
+        result = f'Illegal operator {op}'
+
+    return f'{n1} {op} {n2} = {result}'   # return isn't a function -- no (), but they don't hurt
+calc()

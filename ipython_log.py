d115575ce96d4efd29863500108c533febfe9047
Reuven Lerner <reuven@lerner.co.il>
2023-08-25 16:20:03 +0300

    gitautosync update 299

--- ipython_log.py
+++ ipython_log.py
@@ -4,3 +4,7 @@
 import random
 # what did we get in the module object?
 type(random)
+# what can we do with a module object?
+# not much! We can retrieve items from its attributes:
+
+random.randint(0, 10)    # because "randint" is a function defined in the "random" module

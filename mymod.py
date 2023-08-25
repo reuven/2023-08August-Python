8489c827bb5e83d74682ead102a031fc675cc994
Reuven Lerner <reuven@lerner.co.il>
2023-08-25 17:18:19 +0300

    gitautosync update 361

--- mymod.py
+++ mymod.py
@@ -1,4 +1,5 @@
-print(f'Hello from {__name__}!')
+if __name__ == '__main__':
+    print(f'Hello from {__name__}!')
 
 x = 100
 
@@ -7,4 +8,5 @@ y = [10, 20, 30]
 def hello(name):
     return f'Hello, {name}!'
 
-print(f'Goodbye from {__name__}!')
+if __name__ == '__main__':
+    print(f'Goodbye from {__name__}!')

177eb8b4e98bf84b0e069e36094f44a09d2fd263
Reuven Lerner <reuven@lerner.co.il>
2023-08-25 16:30:12 +0300

    gitautosync update 307

--- ipython_log.py
+++ ipython_log.py
@@ -14,3 +14,12 @@ random.randint(0, 10)    # because "randint" is a function defined in the "rando
 random.randint(0, 10)    # because "randint" is a function defined in the "random" module
 # what if you don't know what attributes are available?
 dir(random)
+# given a string, count the punctuation marks
+s = 'He is really a what?!?'
+total = 0
+
+for one_character in s:
+    if one_character in string.punctuation:
+        total += 1
+
+print(total)
